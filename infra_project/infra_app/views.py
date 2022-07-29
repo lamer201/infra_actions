43cab142d9c7004fd63cbb4b909ea2c55cee9024
Lamer <lamer@localhost>
2022-07-29 22:50:46 +0500

    fix bugs

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 
 
+
 def index(request):
     return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница!')
